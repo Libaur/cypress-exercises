986aed80049718cb5a79a329a4d51f65b7c15f40
Либаур Александр Семенович <ALibaur@tsc.ts>
2024-12-14 13:21:40 +0300

    did refactor

--- data-heroes-test-frontend-autotests/cypress/e2e/looking-for-rick.cy.js
+++ data-heroes-test-frontend-autotests/cypress/e2e/looking-for-rick.cy.js
@@ -1,19 +1,34 @@
 describe('looking for Rick', () => {
+  let rick, mainPage, searchByName, applyButton;
+
+  before(() => {
+    const links = cy.fixture('links');
+    const dataText = cy.fixture('data-text');
+    const elementsText = cy.fixture('elements-text');
+
+    Promise.all([links, dataText, elementsText]).then(([links, dataText, elementsText]) => {
+      rick = dataText.rick;
+      mainPage = links.mainPage;
+      searchByName = elementsText.searchByName;
+      applyButton = elementsText.applyButton;
+    })
+  })
+
   it('passes', () => {
-    cy.visit('https://libaur.github.io/data-heroes-test/')
+    cy.visit(mainPage)
 
-    cy.get('.v-field__field').click({ multiple: true });
+    cy.get('.v-field__input').last().click(); // пара элементов, соответствующих селектору: last() для избежания { multiple: true }
 
-    cy.contains('По имени').click({waitForAnimations: false});
+    cy.get('.v-list-item-title').contains(searchByName).click();
 
-    cy.get('#input-0').type('Rick');
+    cy.get('#input-0').type(rick);
 
-    cy.contains('Применить').click();
+    cy.get('.v-btn').contains(applyButton).click();
 
-    cy.wait(500);
+    cy.wait(500); // загрузка карточек
 
-    cy.get('.v-card').each(($card) => {
-      cy.wrap($card).contains('Rick');
+    cy.get('.v-card').should('have.length', 21).each(($card) => {
+      expect($card).to.contain(rick);
     })
   })
 })
